2fcaa9982dd61963d6cdb691f89a0e4e0a77a8c4
dinnosaur <dinnoroni@gmail.com>
2020-10-04 19:56:39 +0100

    updatedNPMpackages

--- src/Components/journey/Journey.js
+++ src/Components/journey/Journey.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react"
 import Bio from './bio';
+import {Link } from 'react-router-dom';
 import TrackList from './TrackList';
 import Analytics from './Analytics';
 
@@ -26,16 +27,18 @@ class Journey extends Component {
                 <TrackList tracks={this.state.tracks} />
                 <Analytics tracks={this.state.tracks} />
                 {/* <Example tracks={this.state.tracks} /> */}
-                <button class="uk-button uk-button-default uk-margin-small-right" type="button" data-uk-toggle="target: #offcanvas-slide">Slide</button>
-                <div id="offcanvas-slide" data-uk-offcanvas="overlay: true">
-                    <div class="uk-offcanvas-bar">
-
-                        <button class="uk-offcanvas-close" type="button" data-uk-close></button>
-
-                        <h3>Title</h3>
-
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-
+                <button class="uk-button uk-button-default uk-margin-small-right" type="button" data-uk-toggle="target: #modal-full">Open</button>
+            
+                <div id="modal-full" class="uk-modal-full" data-uk-modal>
+                    <div class="uk-modal-dialog">
+                        <button class="uk-modal-close-full uk-close-large" type="button" data-uk-close></button>
+                        <div class="uk-grid-collapse uk-child-width-1-2@s uk-flex-middle" data-uk-grid>
+                            <div class="uk-background-cover" data-uk-height-viewport></div>
+                            <div class="uk-padding-large">
+                                <h1>Headline</h1>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
