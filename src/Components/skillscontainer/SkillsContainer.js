db6d39a01a76fbfc96fc0a56f35b1cc48da0f190
dinnosaur <48298791+dinnosaur@users.noreply.github.com>
2022-01-17 21:09:19 +0000

    minor changes

--- src/Components/skillscontainer/SkillsContainer.js
+++ src/Components/skillscontainer/SkillsContainer.js
@@ -1,17 +1,15 @@
-import React, { Component, Fragment } from "react"
-import SkillList from "./skillsList"
-import SkillFilter from "./skillFilter"
-import ShowSkill from "../showSkill"
+import React, { Component } from 'react';
+import SkillList from './skillsList';
+import SkillFilter from './skillFilter';
 
-
-import API from "../../API.js"
+import API from '../../API.js';
 
 class SkillsContainer extends Component {
   state = {
     skills: [],
     filterSkills: [],
-    active: "All"
-  }
+    active: 'All'
+  };
 
   componentDidMount() {
     API.fetchAllSkills()
@@ -20,44 +18,52 @@ class SkillsContainer extends Component {
         this.setState({
           skills: data,
           filterSkills: data
-        }))
+        })
+      );
   }
 
-
   handleTopic = ({ target: { innerText } }) => {
-    const topicSkills = this.state.skills.filter(skill => skill.topics[0].name === innerText)
+    const topicSkills = this.state.skills.filter(
+      skill => skill.topics[0].name === innerText
+    );
     this.setState({
       filterSkills: topicSkills,
       active: innerText
-    })
-  }
+    });
+  };
 
   showAll = () => {
     this.setState({
       filterSkills: this.state.skills,
-      active: "All"
-    })
-  }
+      active: 'All'
+    });
+  };
 
   handleLength = ({ target: { innerText } }) => {
-    const state = innerText === "Short" ? true : false
-    const lengthTopics = this.state.skills.filter(skill => skill.short_term === state)
+    const state = innerText === 'Short' ? true : false;
+    const lengthTopics = this.state.skills.filter(
+      skill => skill.short_term === state
+    );
 
     this.setState({
       filterSkills: lengthTopics,
       active: innerText
-    })
-  }
+    });
+  };
 
   render = () => {
     return (
       <div class="uk-container uk-container-expand">
-        <SkillFilter active={this.state.active} showAll={this.showAll} handleLength={this.handleLength} handleTopic={this.handleTopic} />
+        <SkillFilter
+          active={this.state.active}
+          showAll={this.showAll}
+          handleLength={this.handleLength}
+          handleTopic={this.handleTopic}
+        />
         <SkillList skills={this.state.filterSkills} />
       </div>
     );
-
-  }
+  };
 }
 
-export default SkillsContainer
+export default SkillsContainer;
